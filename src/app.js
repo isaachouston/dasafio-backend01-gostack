18136b06cfb728fffdeb62734e559108470ec005
isaachouston <isaacprh@gmail.com>
2020-04-13 16:53:47 -0300

    Desafio backend 01 Bootcamp GoStack

---
+++ src/app.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const repositories = [];
+
+function ValidationId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Invalid repository ID!" });
+  }
+
+  return next();
+}
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put("/repositories/:id", ValidationId, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exists" });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  };
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", ValidationId, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", ValidationId, (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  repository.likes += 1;
+
+  return response.json(repository);
+});
+
+module.exports = app;
